02452de91e945d5de99c7f363cae2e90cf640ea9
Anu Tomy <anuatomy@gmail.com>
2017-07-12 04:11:00 +0000

    Finish Version2-Functions

--- wnc-lesson1.js
+++ wnc-lesson1.js
@@ -1,6 +1,24 @@
-var todos = ['item1','item2','item3']
-todos.push('item4','item5')
-todos[0]='item1 updated'
-todos.splice(0,1)
-todos.splice(3,1)
-console.log('Mytodos:',todos)
+var todos = ['item1','item2','item3'];
+//Function to display todos.
+function displayTodos(){
+  console.log('Mytodos:', todos);  
+}
+//Function to add todos.
+function addTodo(todo){
+    todos.push(todo);
+    displayTodos();
+}
+
+//Function to change todos.
+function changeTodo(position, newValue){
+  todos[position] = newValue;
+  displayTodos();
+}
+  
+//Function to delete todos.
+function deleteTodo(position){
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+addTodo('item4')
